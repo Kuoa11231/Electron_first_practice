60d95e6183541a48dd61b5aad35e157f903293ad
Kuoa11231 <Kuoa11231@gmail.com>
2023-09-23 15:45:56 +0800

    0923

---
+++ forge.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  packagerConfig: {
+    asar: true,
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {},
+    },
+    {
+      name: "@electron-forge/maker-rpm",
+      config: {},
+    },
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          type: "git",
+          url: "https://github.com/Kuoa11231/Electron_first_practice.git",
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+  ],
+};
